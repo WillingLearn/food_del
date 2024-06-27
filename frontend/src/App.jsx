3fedc7da7609905fc2a0aa01a997cd4748e514a8
WillingLearn <34353944+WillingLearn@users.noreply.github.com>
2024-06-27 13:36:51 -0400

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Cart from './pages/Cart/Cart'
+import Footer from './components/Footer/Footer'
+import Home from './pages/Home/Home'
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
+import LoginPopup from './components/LoginPopup/LoginPopup'
+
+
+const App = () => {
+  const[showLogin, setShowLogin] = React.useState(false)
+
+  return (
+    <>
+    {showLogin?<LoginPopup setShowLogin={setShowLogin}/>:<></>}
+    <div className='app'>
+    
+    <Navbar setShowLogin={setShowLogin}/>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/cart' element={<Cart/>} />
+      <Route path='/order' element={<PlaceOrder/>}/>
+    </Routes>
+    </div>
+    <Footer/>
+  
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+//import { useContext } from 'react'
+import { StoreContext } from '../../context/StoreContext'
+import FoodItem from '../FoodItem/FoodItem'
+import './FoodDisplay.css'
+
+//console.log(1);
+
+const FoodDisplay = ({category}) => {
+    const {food_list} = React.useContext(StoreContext)
+    return (
+
+    <div className='food-display' id='food-display'>
+        <h2>Top Dishes Near you</h2>
+        <div className="food-display-list">
+
+            {food_list.map((item,index)=>{
+                if (category==="All" || category===item.category){
+                    return<FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image} />
+
+                }
+            })}
+        </div>
+    </div>
+    )
+}
+export default FoodDisplay
